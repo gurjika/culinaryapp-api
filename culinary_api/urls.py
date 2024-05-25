8f324c871705e1a1869058f7f441841b6e6f6fcf
Luka <lgurjidze@gmail.com>
2024-05-25 18:16:02 +0400

    add debug toolbar

--- culinary_api/urls.py
+++ culinary_api/urls.py
@@ -20,6 +20,8 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('auth/', include('core.urls')),
-    path('api/', include('culinaryapp.urls'))
-
+    path('api/', include('culinaryapp.urls')),
+    path("__debug__/", include("debug_toolbar.urls")),
 ]
+
+
