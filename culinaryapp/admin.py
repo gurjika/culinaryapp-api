20917bef2b335f004ce49987cbbf5adf4c07012c
Luka <lgurjidze@gmail.com>
2024-05-27 19:14:40 +0400

    register models in admin panel

--- culinaryapp/admin.py
+++ culinaryapp/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from . import models
 # Register your models here.
+
+
+admin.site.register(models.UserProfile)
+admin.site.register(models.Dish)
+admin.site.register(models.Ingredient)
+admin.site.register(models.Rating)
+admin.site.register(models.DishTypeTag)
--- culinaryapp/models.py
+++ culinaryapp/models.py
@@ -7,14 +7,24 @@ from django.core.validators import MaxValueValidator, MinValueValidator
 class UserProfile(models.Model):
     user = models.OneToOneField('auth.user', on_delete=models.CASCADE, related_name='profile')
 
+    def __str__(self) -> str:
+        return f'{self.user.first_name} {self.user.last_name}'
+
 class Ingredient(models.Model):
     title = models.CharField(max_length=255)
+
+    def __str__(self) -> str:
+        return self.title
 
 class Dish(models.Model):
     title = models.CharField(max_length=255)
     profile = models.ForeignKey(UserProfile, on_delete=models.CASCADE, related_name='dishes')
     receipe = models.TextField()
     ingredient = models.ManyToManyField(Ingredient, through='DishIngredient')
+
+
+    def __str__(self) -> str:
+        return self.title
     
 
 class DishIngredient(models.Model):
@@ -41,10 +51,16 @@ class Rating(models.Model):
     dish = models.ForeignKey(Dish, on_delete=models.CASCADE, related_name='ratings')
     rater = models.ForeignKey(UserProfile, on_delete=models.CASCADE, related_name='rateds')
 
+    def __str__(self) -> str:
+        return f'{self.rater} - {self.rating}'
+
 
 class DishTypeTag(models.Model):
     dish = models.ManyToManyField(Dish, through='DishTag')
     dish_tag = models.CharField(max_length=255)
+
+    def __str__(self) -> str:
+        return self.dish_tag
 
 class DishTag(models.Model):
     dish = models.ForeignKey(Dish, on_delete=models.CASCADE, related_name='dish_tags')
