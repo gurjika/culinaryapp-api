6c5121df0baf9864aab708bd260a952cbd1c5943
Luka <lgurjidze@gmail.com>
2024-07-14 17:24:06 +0400

    config nginx and gunicorn

--- culinary_api/settings.py
+++ culinary_api/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -146,7 +146,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
---
+++ gunicorn_config.py
@@ -0,0 +1,6 @@
+bind = "0.0.0.0:8000"  # Address and port to bind
+workers = 3  # Number of worker processes
+accesslog = "-"  # Log access to stdout
+errorlog = "-"  # Log errors to stdout
+loglevel = "info"  # Logging level
+timeout = 120  
