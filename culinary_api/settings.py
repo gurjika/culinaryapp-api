45e8bce1520a6e32779630abeb477d976413cfda
Luka <lgurjidze@gmail.com>
2024-07-26 20:24:47 +0400

    debug true for testing

--- culinary_api/settings.py
+++ culinary_api/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 # SECURITY WARNING: keep the secret key used in production secret!
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['207.154.236.26', '127.0.0.1']
 
